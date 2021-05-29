08e27c8356ac0833200a32d3ebc7cd8d836ff151
Crowdbotics <team@crowdbotics.com>
2021-05-29 01:25:54 +0000

    Initial commit.

---
+++ cheeky_peazy_27539/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for cheeky_peazy_27539 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cheeky_peazy_27539.settings')
+
+application = get_wsgi_application()
